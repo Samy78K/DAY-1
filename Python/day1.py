91b6ef8fe82afb7b7b18891ab9d21e2e52230cf1
Samy78K <khalisamy72@gmail.com>
2025-02-25 09:51:33 +0100

    Add files via upload

---
+++ Python/day1.py
@@ -0,0 +1,100 @@
+#Afficher un text ! 
+
+#print("Bonjour, bienvenue dans le cours de Python !")#
+
+#-------------------------------------
+
+
+#faire une somme !
+
+#a = input("Valeur de a :")#
+#b = input("Valeur de b :")#
+
+# somme = int(a) + int(b)#
+
+#print("le resultat :", somme)#
+
+#-------------------------------------------------------
+
+
+#Pair ou Impair !
+
+# n = input("Taper la Valeur de n : ")
+# n =int(n)
+# r = n % 2
+# if( r == 0 ):
+#     print("Le nombre n Taper est Pair")
+    
+# else:
+    # print("Le nombre est Impair")
+
+#--------------------------------------------------
+
+#Supérieur, inférieur ou egal a 10 ! 
+
+# n = input("taper votre nombre : ")
+# n=int(n)
+# if(n>=10):
+#     print("ceci est plus grand ou egal a 10 !")
+# else:
+#     print("ceci est inferieur a 10 !")
+
+
+#Calcul d'age ! 
+
+# a = int(input("Saisir l'année de naissance"))
+# ac =int(input("Saisir l'année actuelle"))
+# age=ac-a
+# print("tu a", age,"ans")
+
+
+
+#Comparaison nombre ! 
+
+# a = (input("Entrer votre Premier Nombre :"))
+# b = (input("Entrer votre deuxième nombre : "))
+
+# if a == b:
+#     print("a est égal a b ")
+
+# elif a > b : 
+#     print("a est superieur a b")
+
+# elif a < b :
+#     print("a est inferieur a b")
+
+# else :
+#     print("a est different de b")
+
+
+#------------------------------------------------------------------
+
+#La multiplication ( produit ) calculer l'air d'un rectangle
+
+# la = input("Rentre la largeur :")
+# Lo = input("Rentre la Longueur :")
+
+# produit = int(la) * int(Lo)
+
+# print("L’aire du rectangle est :", produit)
+
+
+#-------------------------------------------------
+
+#Calculer la Moyenne des notes
+
+# note1 = int(input("Première Note"))
+# note2 = int(input("Deuxième Note"))
+# note3 = int(input("Troisième Note"))
+
+# sommenotes = int(note1) + int(note2) + int(note3) 
+
+# sommenotes = [note1,note2,note3]
+
+# allnotes = sum(sommenotes)/len(sommenotes)
+
+
+# if allnotes>=10:
+#     print("tu es admis :)")
+# else:
+#     print("tu es refusé :(")
